e0b98b82ede8d321cd9546a1112eabc6c62a8bc4
alexisvannson <vannson.alexis@gmail.com>
2024-12-04 14:51:43 +0100

    tkt mon reuf

---
+++ get_next_line_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avannson  <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 14:21:34 by avannson          #+#    #+#             */
+/*   Updated: 2024/12/04 14:35:46 by avannson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1
+
+void	ft_putstr(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		write(1, &src[i], 1);
+		i++;
+	}
+}
+
+void    ft_display_line(char *filename)
+{
+	int		fd;
+	int		read_items;
+	char	buffer[BUFFER_SIZE];
+
+	read_items = 1;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("Cannot read file.\n");
+		return ;
+	}
+	while (read_items > '\n')
+	{
+		read_items = read(fd, buffer, BUFFER_SIZE);
+		write(1, buffer, read_items);
+	}
+	if (read_items == -1)
+	{
+		close(fd);
+		ft_putstr("Cannot read file.\n");
+	}
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		ft_putstr("File name missing.\n");
+		return (-1);
+	}
+	if (argc > 2)
+	{
+		ft_putstr("Too many arguments.\n");
+		return (-1);
+	}
+	ft_display_line(argv[1]);
+	return (0);
+}
